65dd0d0d5ad1983e1f08c070b3b0662c0450c01b
Matej-Omazic <omazzza@gmail.com>
2024-01-31 18:00:20 +0100

    Contact page updated and fixed typo for routing.

--- sweet-wish-ui/src/components/Navbar.svelte
+++ sweet-wish-ui/src/components/Navbar.svelte
@@ -22,7 +22,6 @@
 
         <div class="md:hidden">
             <button on:click={toggleMobileMenu} class="text-pink-500 hover:text-pink-300 focus:outline-none transition">
-                <!-- Hamburger icon for mobile -->
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                 </svg>
@@ -39,7 +38,7 @@
         {#if isMobileMenuOpen}
             <div in:slide="{{ duration: 300 }}" class="md:hidden absolute top-16 right-4 bg-white border rounded-md shadow-md">
                 <a href="/" class="bg-gray-500 block px-4 py-2 hover:text-pink-100">Početna</a>
-                <a href="/product" class="bg-gray-500 block px-4 py-2 hover:text-pink-100">Proizvodi</a>
+                <a href="/products" class="bg-gray-500 block px-4 py-2 hover:text-pink-100">Proizvodi</a>
                 <a href="/about" class="bg-gray-500 block px-4 py-2 hover:text-pink-100">O nama</a>
                 <a href="/contact" class="bg-gray-500 block px-4 py-2 hover:text-pink-100">Kontakt</a>
             </div>
