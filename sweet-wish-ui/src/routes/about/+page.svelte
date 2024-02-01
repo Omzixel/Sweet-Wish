2b402437dee86b85b8c984af0c6e4357354b03d8
Matej-Omazic <omazzza@gmail.com>
2024-02-01 17:55:37 +0100

    About US page updated.

--- sweet-wish-ui/src/routes/about/+page.svelte
+++ sweet-wish-ui/src/routes/about/+page.svelte
@@ -1,9 +1,37 @@
-<section class="py-16 bg-[#fae3eb]">
-    <div class="container mx-auto ">
-        <h1 class="text-4xl font-bold mb-8">About Page</h1>
+<section class="py-16 bg-[#fae3eb] flex items-center justify-center">
+    <div class="container mx-auto flex flex-col-reverse md:flex-row items-center justify-center">
+        <div class="w-full md:w-1/2 pr-0 md:pr-8 mb-8 md:mb-0">
+            <img class="rounded-lg w-full"
+                 src="https://i.ibb.co/6NDwF7d/413979415-856362963159825-8024410639689083445-n.jpg"
+                 alt="413979415-856362963159825-8024410639689083445-n"
+                 border="0">
+        </div>
+        <div class="w-full md:w-1/2 pl-0 md:pl-8">
+            <h1 class="text-4xl font-bold mb-8">About Page</h1>
+            <div class="text-gray-700 p-8">
+                <p class="mb-4">
+                    Dobrodošli u <span class="font-bold">Slatka Želju</span> - mjesto gdje se ostvaruju vaše najslađe želje!
+                </p>
+                <p class="mb-4">
+                    Naša priča započinje s strašću prema slastičarstvu i ljubavlju prema stvaranju savršenih okusa.
+                    <span class="font-bold">Slatka Želja</span> je obiteljska tvrtka posvećena proizvodnji vrhunskih kolača
+                    i torti koje će zadovoljiti sva vaša nepca.
+                </p>
+                <p class="mb-4">
+                    Kroz pažljiv odabir najkvalitetnijih sastojaka, naši majstori slastica svakodnevno stvaraju čaroliju koja će
+                    oduševiti vaše nepce i pružiti vam nezaboravno iskustvo uživanja u slatkim delicijama.
+                    Posebnu pažnju posvećujemo detaljima i estetici, kako bismo svaki kolač ili tortu pretvorili u pravo malo umjetničko djelo.
+                </p>
+                <p class="mb-4">
+                    Bez obzira trebate li slatkiš za posebnu prigodu, slavlje ili jednostavno želite uživati u trenutku
+                    slatke radosti, <span class="font-bold">Slatka Želja</span> je tu da ispuni vaše najslađe želje.
+                </p>
+                <p>
+                    Radujemo se što ćemo vas povesti na put slatkog užitka i dijeliti s vama radost stvaranja nezaboravnih
+                    slatkih trenutaka. Hvala vam što ste dio <span class="font-bold">Slatke Želje</span>!
+                </p>
+            </div>
 
-        <p class="text-gray-700">
-            About US.
-        </p>
+        </div>
     </div>
 </section>
